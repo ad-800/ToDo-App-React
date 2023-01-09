db5ee6e0b465a4b3a1c0a19c2dc456073f1ed179
Adalyne Cummins <adalynecummins@gmail.com>
2023-01-08 20:01:49 -0500

    can now add new tasks

--- src/tasks/AddTasks.js
+++ src/tasks/AddTasks.js
@@ -1,27 +1,51 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function AddTasks() {
+  let navigate = useNavigate();
+
+  const [task, setTask] = useState({
+    name:'',
+    status:''
+  });
+
+  const {name, status} = task;
+
+  const onInputChange = (e) => {
+    setTask({...task, [e.target.name]:e.target.value})
+  }
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:8080/task', task);
+    navigate('/');
+  }
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Add New Task</h2>
 
+          <form onSubmit={(e) => onSubmit(e)}>
           <div className='mb-3'>
             <label htmlfor="Name" className="form-label">
               Name
             </label>
-            <input type={"text"} className='form-control' placeholder='Task name' name='name' />
+            <input type={"text"} className='form-control' placeholder='Task name' name='name' value={name} onChange={(e) => onInputChange(e)} />
           </div>
 
           <div className='mb-3'>
             <label htmlfor="Status" className="form-label">
               Status
             </label>
-            <input type={"text"} className='form-control' placeholder='Current task status' name='status' />
+            <input type={"text"} className='form-control' placeholder='Current task status' name='status' value={status} onChange={(e) => onInputChange(e)} />
           </div>
+
           <button type='submit' className='btn btn-success'>Submit</button>
           <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+          </form>
         </div>
       </div>
     </div>
